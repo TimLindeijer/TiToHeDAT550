87a58a47e139a1249e3f784eb26b19aa8a50b26e
Tim UiS Lindeijer <tlindeijer@gmail.com>
2024-04-23 22:45:29 +0200

    made setup cleaner

--- setup.sh
+++ setup.sh
@@ -2,13 +2,11 @@
 #SBATCH --gres=gpu:0
 #SBATCH --partition=gpuA100 
 #SBATCH --time=03:15:00
-#SBATCH --job-name=pip
-#SBATCH --output=pip.out
+#SBATCH --job-name=setup
+#SBATCH --output=outputs/setup.out
  
 # Activate environment
 uenv verbose cuda-11.4.4 cudnn-11.4-8.2.4
 uenv miniconda3-py311
-# conda env remove --name torch550
-# conda env create --file requirements.yaml
-conda activate torch550
-pip install wandb
+conda env create --file requirements.yaml
+
